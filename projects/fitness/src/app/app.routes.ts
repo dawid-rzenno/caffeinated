a1851fbbc90261a5d019c2bd0d62dffacb6c9752
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-05-27 22:33:29 +0200

    Define root routing

--- projects/fitness/src/app/app.routes.ts
+++ projects/fitness/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'food',
+    loadChildren: () => import('./food/food.module').then(m => m.FoodModule)
+  },
+  {
+    path: 'training',
+    loadChildren: () => import('./training/training.module').then(m => m.TrainingModule)
+  },
+];

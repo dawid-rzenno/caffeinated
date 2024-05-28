f3206e88350395dbccde925e8a068973de3bdb1a
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-05-28 13:43:40 +0200

    Display dish details

--- projects/fitness/src/app/food/dish/dish.component.ts
+++ projects/fitness/src/app/food/dish/dish.component.ts
@@ -1,23 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { Dish } from "../dish";
-import { JsonPipe } from "@angular/common";
+import { JsonPipe, NgForOf } from "@angular/common";
+import { Ingredient, Nutrition } from "../ingredient";
 
 @Component({
   selector: 'app-dish',
   standalone: true,
   imports: [
-    JsonPipe
+    JsonPipe,
+    NgForOf
   ],
   templateUrl: './dish.component.html',
   styleUrl: './dish.component.scss'
 })
 export class DishComponent implements OnInit {
-  dish?: Dish = undefined;
+  dish?: Dish;
+  calories?: number;
+  proteins?: number;
+  carbohydrates?: number;
+  fats?: number;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.data.subscribe((data) => this.dish = data['dish'])
+    this.route.data.subscribe((data) => {
+      this.dish = data['dish'];
+
+      if (this.dish) {
+        this.calculateDishNutrition(this.dish)
+      }
+    })
+  }
+
+  calculateDishNutrition(dish: Dish) {
+    this.calories = this.sumIngredientsNutritionInCategory(dish.ingredients, 'calories')
+    this.proteins = this.sumIngredientsNutritionInCategory(dish.ingredients, 'proteins')
+    this.carbohydrates = this.sumIngredientsNutritionInCategory(dish.ingredients, 'carbohydrates')
+    this.fats = this.sumIngredientsNutritionInCategory(dish.ingredients, 'fats')
+  }
+
+  sumIngredientsNutritionInCategory(ingredients: Ingredient[], category: keyof Nutrition): number {
+    return Math.round(
+      ingredients.reduce((sum: number, ingredient: Ingredient) => sum + ingredient[category], 0) * 1000
+    ) / 1000
   }
 }
--- projects/fitness/src/app/food/ingredient.ts
+++ projects/fitness/src/app/food/ingredient.ts
@@ -1,7 +1,10 @@
-export interface Ingredient {
+export interface Ingredient extends Nutrition {
   id: string;
   name: string;
   category: string;
+}
+
+export interface Nutrition {
   calories: number;
   proteins: number;
   carbohydrates: number;

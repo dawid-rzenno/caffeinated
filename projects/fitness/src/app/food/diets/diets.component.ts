13090b42e2da3ba9dd2ca04834c8bfef376d0aa9
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-06-06 12:02:30 +0200

    Generate lists in food module

---
+++ projects/fitness/src/app/food/diets/diets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diets',
+  standalone: true,
+  imports: [],
+  templateUrl: './diets.component.html',
+  styleUrl: './diets.component.scss'
+})
+export class DietsComponent {
+
+}

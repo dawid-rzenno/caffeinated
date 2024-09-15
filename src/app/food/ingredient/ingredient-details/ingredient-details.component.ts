d03ecd85a0ecbecfeca6c3509bbdbedb087061b9
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-09-15 23:02:49 +0200

    Add h1 and h2 for better screen reader experience

--- src/app/food/ingredient/ingredient-details/ingredient-details.component.ts
+++ src/app/food/ingredient/ingredient-details/ingredient-details.component.ts
@@ -3,12 +3,14 @@ import { DetailsComponentAbstract } from "../../details-component.abstract";
 import { IngredientDetails } from "../ingredient";
 import { ActivatedRoute } from "@angular/router";
 import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @Component({
   selector: 'cortado-ingredient-details',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ],
   templateUrl: './ingredient-details.component.html',
   styleUrl: './ingredient-details.component.scss'
